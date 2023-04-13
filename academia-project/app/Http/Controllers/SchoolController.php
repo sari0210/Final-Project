f728f0dd0270ed671bb8e379ccfc773526c97967
Tatiana Iraheta <116608180+TATY1795@users.noreply.github.com>
2023-04-13 17:07:04 -0600

    commit api.php

--- academia-project/app/Http/Controllers/SchoolController.php
+++ academia-project/app/Http/Controllers/SchoolController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\School;
 
 class SchoolController extends Controller
 {
@@ -11,7 +12,9 @@ class SchoolController extends Controller
      */
     public function index()
     {
-        //
+        //TRAER TODOS LOS DATOS DE LAS COLUMNAS SCHOOL  SELECT * from school;
+        $school = School::all();
+        return $school;
     }
 
     /**
@@ -25,9 +28,20 @@ class SchoolController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+
+   //REQUEST = 
+     public function store(Request $request)
     {
-        //
+      // creamos el objeto para guardarlo
+       $school = new School;
+       $school-> school_name = $request->school_name;
+       $school-> director_name = $request->director_name;
+       $school-> school_address = $request->school_address;
+       $school-> phone_number = $request->phone_number;
+        //GUARDAR DATOS EN NUESTRA TABLA 
+       $school-> save();
+       return $school;
+
     }
 
     /**
@@ -51,7 +65,16 @@ class SchoolController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //buscamos la escula si no existe fallamos y no seguimos la ejecucion
+        $school = School::findOrFail($id);
+        // ACTUALIZAR LOS DATOS 
+        $school-> school_name = $request->school_name;
+       $school-> director_name = $request->director_name;
+       $school-> school_address = $request->school_address;
+       $school-> phone_number = $request->phone_number;
+          //GUARDAR DATOS EN NUESTRA TABLA 
+          $school-> save();
+          return $school;
     }
 
     /**
@@ -59,6 +82,8 @@ class SchoolController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        //eliminar datos de nuestra tabla
+        School::destroy($id);
+        return "delete succssfully";
     }
 }
--- academia-project/routes/api.php
+++ academia-project/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SchoolController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// routes laravel 8
+/*
+Route::get('/school','App\Http\Controllers\ArticuloController@index');
+Route::post('/school','App\Http\Controllers\ArticuloController@store');
+Route::put('/school/{id}','App\Http\Controllers\ArticuloController@update');
+Route::delete('/school/{id}','App\Http\Controllers\ArticuloController@destroy');
+*/
+// routes laravel 9-10
+
+ 
+//Routes Laravel 9-10
+
+Route::get('/school',[SchoolController::class,'index']);
+Route::post('/school',[SchoolController::class,'store']);
+Route::put('/school/{id}',[SchoolController::class,'update']);
+Route::delete('/school/{id}',[SchoolController::class,'destroy']);
+
+/*
+Route::get('/',[ArticuloController::class,'index']);
+Route::post('/',[ArticuloController::class,'store']);
+Route::put('//{id}',[ArticuloController::class,'update']);
+Route::delete('//{id}',[ArticuloController::class,'destroy']);
+
+*/
