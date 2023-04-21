dd4a97dd5faad6dd054ef27a66af18b53485e369
Melanie Elías <104339401+Melanie-Elias@users.noreply.github.com>
2023-04-21 01:59:23 -0600

    TeacherController

--- academia-project/app/Http/Controllers/TeacherController.php
+++ academia-project/app/Http/Controllers/TeacherController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Teacher;
 
 class TeacherController extends Controller
 {
@@ -11,7 +12,9 @@ class TeacherController extends Controller
      */
     public function index()
     {
-        //
+        //TRAER TODOS LOS DATOS DE LAS COLUMNAS TEACHER SELECT * from teacher;
+        $teacher = Teacher::all();
+        return $teacher;
     }
 
     /**
@@ -27,7 +30,16 @@ class TeacherController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // creamos el objeto para guardarlo
+        $teacher = new Teacher;
+        $teacher-> name = $request->name;
+        $teacher-> lastname = $request->lastname;
+        $teacher-> address = $request->address;
+        $teacher-> phone_number = $request->phone_number;
+        $teacher-> groups_id = $request->groups_id;
+        //GUARDAR DATOS EN NUESTRA TABLA
+        $teacher-> save();
+        return $teacher;
     }
 
     /**
@@ -51,7 +63,17 @@ class TeacherController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //buscamos al maestro, si no existe fallamos y no seguimos la ejecucion
+        $teacher = Teacher::findOrFail($id);
+        // ACTUALIZAR LOS DATOS
+        $teacher-> name = $request->name;
+        $teacher-> lastname = $request->lastname;
+        $teacher-> address = $request->address;
+        $teacher-> phone_number = $request->phone_number;
+        $teacher-> groups_id = $request->groups_id;
+        // GUARDAR DATOS EN NUESTRA TABLA
+        $teacher-> save();
+        return $teacher;
     }
 
     /**
@@ -59,6 +81,8 @@ class TeacherController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        //eliminar datos de nuestra tabla
+        Teacher::destroy($id);
+        return "delete successfully";
     }
 }
--- academia-project/routes/api.php
+++ academia-project/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\SchoolController;
 use App\Http\Controllers\GroupsController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TeacherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,3 +55,8 @@ Route::get('/admin',[AdminController::class,'index']);
 Route::post('/admin',[AdminController::class,'store']);
 Route::put('/admin/{id}',[AdminController::class,'update']);
 Route::delete('/admin/{id}',[AdminController::class,'destroy']);
+
+Route::get('/teacher',[TeacherController::class,'index']);
+Route::post('/teacher',[TeacherController::class,'store']);
+Route::put('/teacher/{id}',[TeacherController::class,'update']);
+Route::delete('/teacher/{id}',[TeacherController::class,'destroy']);
